d949ea43d0791924e4bebc692a15fec7d8e80876
harjinder singh <harjindersingh1123@gmail.com>
2018-08-01 11:38:15 -0700

    1) Fixed bug for empty leaf 2) rename files. 3) added new unit test case

--- YangXmlDiffDriver.py
+++ YangXmlDiffDriver.py
@@ -19,6 +19,14 @@ def my_node_compare(expected_node, actual_node):
         return False
     expected_tag = expected_node.tag.strip()
     actual_tag   = actual_node.tag.strip()
+   
+    # if both none return true
+    if expected_node.text is None and actual_node.text is None:
+        return True
+    #if only one None, then return mismatch False
+    if expected_node.text is None or actual_node.text is None:
+        print ("leaf {a} or  {b} is empty".format(a=actual_tag, b=expected_tag))
+        return False
     
     expected_text = expected_node.text.strip()
     actual_text   = actual_node.text.strip()
@@ -36,6 +44,12 @@ class TestXml_diff(unittest.TestCase):
    
     
     def test_one_positive(self):
+        yangXmlDiff = xmldiff.YangXmlDiff(my_node_compare, verbose=1)
+        result = yangXmlDiff.compare_root(expected, actual)
+        self.assertEqual(result, True)
+    def test_one_positive_empty_type(self):
+        expected = ET.parse('test_xml/expected_1.txt')
+        actual = ET.parse('test_xml/actual_1.txt')
         yangXmlDiff = xmldiff.YangXmlDiff(my_node_compare, verbose=1)
         result = yangXmlDiff.compare_root(expected, actual)
         self.assertEqual(result, True)
